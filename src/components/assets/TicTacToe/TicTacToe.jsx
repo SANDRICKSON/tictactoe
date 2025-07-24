1d845b8fb12056a3dd3e8143ced64e2478a70517
SANDRICKSON <sandroqatamadze802@gmail.com>
2025-07-24 16:33:40 +0400

    2

--- src/components/assets/TicTacToe/TicTacToe.jsx
+++ src/components/assets/TicTacToe/TicTacToe.jsx
@@ -1,111 +1,88 @@
 import React from 'react';
-import './TicTacToe.css'
-import circle_icon from '../circle.png'
-import cross_icon from '../cross.png'
-
-let data = ["", "", "", "", "", "", "", "", ""]
+import './TicTacToe.css';
+import circle_icon from '../circle.png';
+import cross_icon from '../cross.png';
 
 const TicTacToe = () => {
-    let [count, setCount] = React.useState(0);
-    let [lock, setLock] = React.useState(false);
-    let titleRef = React.useRef(null);
-    let box1 = React.useRef(null);
-    let box2 = React.useRef(null);
-    let box3 = React.useRef(null);
-    let box4 = React.useRef(null);
-    let box5 = React.useRef(null);
-    let box6 = React.useRef(null);
-    let box7 = React.useRef(null);
-    let box8 = React.useRef(null);
-    let box9 = React.useRef(null);
+    const [data, setData] = React.useState(Array(9).fill(""));
+    const [isXTurn, setIsXTurn] = React.useState(true);
+    const [lock, setLock] = React.useState(false);
+    const titleRef = React.useRef(null);
+    const boxRefs = Array(9).fill(null).map(() => React.useRef(null));
 
-    let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
+    const handleClick = (index) => {
+        if (lock || data[index] !== "") return;
 
-    const toggle = (e, num) => {
-        if (lock || data[num] !== "") {
-            return;
+        const newData = [...data];
+        newData[index] = isXTurn ? "x" : "o";
+        boxRefs[index].current.innerHTML = `<img src='${isXTurn ? cross_icon : circle_icon}' />`;
+
+        setData(newData);
+        setIsXTurn(!isXTurn);
+        checkWin(newData);
+    };
+
+    const checkWin = (currentData) => {
+        const winPatterns = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+        ];
+
+        for (let pattern of winPatterns) {
+            const [a, b, c] = pattern;
+            if (currentData[a] && currentData[a] === currentData[b] && currentData[b] === currentData[c]) {
+                declareWinner(currentData[a]);
+                return;
+            }
         }
 
-        if (count % 2 === 0) {
-            e.target.innerHTML = `<img src='${cross_icon}'>`;
-            data[num] = "x";
-            setCount(++count);
-        } else {
-            e.target.innerHTML = `<img src='${circle_icon}'>`;
-            data[num] = "o";
-            setCount(++count);
-        }
-
-        checkwin();
-    }
-
-    const checkwin = () => {
-        if (data[0] === data[1] && data[1] === data[2] && data[0] !== "") {
-            won(data[0]);
-        } else if (data[3] === data[4] && data[4] === data[5] && data[3] !== "") {
-            won(data[3]);
-        } else if (data[6] === data[7] && data[7] === data[8] && data[6] !== "") {
-            won(data[6]);
-        } else if (data[0] === data[3] && data[3] === data[6] && data[0] !== "") {
-            won(data[0]);
-        } else if (data[1] === data[4] && data[4] === data[7] && data[1] !== "") {
-            won(data[1]);
-        } else if (data[2] === data[5] && data[5] === data[8] && data[2] !== "") {
-            won(data[2]);
-        } else if (data[0] === data[4] && data[4] === data[8] && data[0] !== "") {
-            won(data[0]);
-        } else if (data[2] === data[4] && data[4] === data[6] && data[2] !== "") {
-            won(data[2]);
-        } else if (!data.includes("")) {
-            draw();
+        if (!currentData.includes("")) {
+            declareDraw();
         }
     };
 
-    const won = (winner) => {
+    const declareWinner = (winner) => {
         setLock(true);
-        if (winner === "x") {
-            titleRef.current.innerHTML = `Congratulations! Winner is <img src=${cross_icon}>`;
-        } else {
-            titleRef.current.innerHTML = `Congratulations! Winner is <img src=${circle_icon}>`;
-        }
-    }
+        titleRef.current.innerHTML = `Winner is <img src=${winner === "x" ? cross_icon : circle_icon} />`;
+    };
 
-    const draw = () => {
+    const declareDraw = () => {
         setLock(true);
         titleRef.current.innerHTML = `It's a Draw!`;
-    }
+    };
 
     const reset = () => {
+        setData(Array(9).fill(""));
+        setIsXTurn(true);
         setLock(false);
-        data = ["", "", "", "", "", "", "", "", ""];
         titleRef.current.innerHTML = 'TIC TAC TOE In <span>React</span>';
-        box_array.map((e) => {
-            e.current.innerHTML = "";
+        boxRefs.forEach(ref => {
+            if (ref.current) ref.current.innerHTML = "";
         });
-        setCount(0);
-    }
+    };
 
     return (
         <div className="container">
             <h1 className="title" ref={titleRef}>TIC TAC TOE In <span>React</span></h1>
             <div className="board">
-                <div className="row1">
-                    <div className="boxes" ref={box1} onClick={(e) => toggle(e, 0)}></div>
-                    <div className="boxes" ref={box2} onClick={(e) => toggle(e, 1)}></div>
-                    <div className="boxes" ref={box3} onClick={(e) => toggle(e, 2)}></div>
-                </div>
-                <div className="row2">
-                    <div className="boxes" ref={box4} onClick={(e) => toggle(e, 3)}></div>
-                    <div className="boxes" ref={box5} onClick={(e) => toggle(e, 4)}></div>
-                    <div className="boxes" ref={box6} onClick={(e) => toggle(e, 5)}></div>
-                </div>
-                <div className="row3">
-                    <div className="boxes" ref={box7} onClick={(e) => toggle(e, 6)}></div>
-                    <div className="boxes" ref={box8} onClick={(e) => toggle(e, 7)}></div>
-                    <div className="boxes" ref={box9} onClick={(e) => toggle(e, 8)}></div>
-                </div>
+                {[0, 1, 2].map(row => (
+                    <div key={row} className={`row${row + 1}`}>
+                        {[0, 1, 2].map(col => {
+                            const index = row * 3 + col;
+                            return (
+                                <div
+                                    key={index}
+                                    className="boxes"
+                                    ref={boxRefs[index]}
+                                    onClick={() => handleClick(index)}
+                                ></div>
+                            );
+                        })}
+                    </div>
+                ))}
             </div>
-            <button className="reset" onClick={() => { reset() }}>Reset</button>
+            <button className="reset" onClick={reset}>Reset</button>
         </div>
     );
 };
